ce1894712d350e84ba024941f31da96865f9b0be
Anders Ryberg <anders@razlogic.se>
2023-04-12 05:44:27 +0200

    pi7

--- build/linux_x64/build.sh
+++ build/linux_x64/build.sh
@@ -1,4 +1,4 @@
 cd ../../flutter
 flutter build linux
 cd ../build/linux_x64
-tar -czvf oxigen_protocol_explorer_3-linux-x64.tar.gz --directory=../../flutter/build/linux/x64/release/bundle .
+tar -czvf oxigen_protocol_explorer_3_linux_x64.tar.gz --directory=../../flutter/build/linux/x64/release/bundle .
