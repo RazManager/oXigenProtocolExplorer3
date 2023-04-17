f73a474161dd531b4ae762af36c2dffe172697ca
Anders Ryberg <anders@razlogic.se>
2023-04-17 06:45:15 +0200

    dotnet macos build

--- build/dotnet/build.ps1
+++ build/dotnet/build.ps1
@@ -3,6 +3,5 @@ dotnet publish ../../dotnet/oXigenProtocolExplorer3/oXigenProtocolExplorer3.cspr
 dotnet publish ../../dotnet/oXigenProtocolExplorer3/oXigenProtocolExplorer3.csproj --configuration Release --output linux-x64 --self-contained --runtime linux-x64
 dotnet publish ../../dotnet/oXigenProtocolExplorer3/oXigenProtocolExplorer3.csproj --configuration Release --output win-x64 --self-contained --runtime win-x64
 dotnet publish ../../dotnet/oXigenProtocolExplorer3/oXigenProtocolExplorer3.csproj --configuration Release --output win-x86 --self-contained --runtime win-x86
-dotnet publish ../../dotnet/oXigenProtocolExplorer3/oXigenProtocolExplorer3.csproj --configuration Release --output osx-x64 --self-contained --runtime osx-x64
 
 pause
---
+++ build/dotnet/build.sh
@@ -0,0 +1,1 @@
+dotnet publish ../../dotnet/oXigenProtocolExplorer3/oXigenProtocolExplorer3.csproj --configuration Release --output osx-x64 --self-contained --runtime osx-x64
