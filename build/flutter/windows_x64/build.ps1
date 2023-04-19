d899402e0f9a1ba0e5fd0394c9ab77f042788a81
Anders Ryberg <anders@razlogic.se>
2023-04-19 18:53:15 +0200

    windows_x64 build

--- build/flutter/windows_x64/build.ps1
+++ build/flutter/windows_x64/build.ps1
@@ -1,6 +1,6 @@
 cd ../../../flutter
 flutter build windows
 cd ../build/flutter/windows_x64
-Compress-Archive -Force -Path ../../../flutter/build/windows/runner/Release/* -DestinationPath oxigen_protocol_explorer_3.zip
+Compress-Archive -Force -Path ../../../flutter/build/windows/runner/Release/* -DestinationPath oxigen_protocol_explorer_3_windows_x64.zip
 
 pause
